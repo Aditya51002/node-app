c5bbcaa573f62f0c91db637397c56fed8e9a8158
Aditya51002 <adityamaurya510@gmail.com>
2026-05-06 15:05:14 +0530

    fix: update welcome message in root route response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello Dhruva Welcome to Express.js');
 })
  app.listen(PORT, () => {
     console.log(`Server is running on  https://localhost:${PORT}`);
